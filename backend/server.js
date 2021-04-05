fa0f197228ae2b741d45ea6b8a8ec00f930a03d2
Carlos Orelhas <c.orelhas@gmail.com>
2021-04-05 22:27:51 +0100

    Implement server & create folders + user models

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+
+// IMPORT ROUTES
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// APP USE ROUTES
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
+);
