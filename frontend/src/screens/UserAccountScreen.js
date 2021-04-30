979a97cfe98d602d33aa30f3d19095fdb65bf343
Carlos Orelhas <c.orelhas@gmail.com>
2021-04-30 17:44:56 +0100

    Get bike details to show in the table

--- frontend/src/screens/UserAccountScreen.js
+++ frontend/src/screens/UserAccountScreen.js
@@ -46,75 +46,109 @@ const UserAccount = ({ location, history }) => {
   };
 
   return (
-    <Row>
-      <Col md={3} className="mt-2">
-        <h3>Dados Pessoais</h3>
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="username">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="username"
-              placeholder="Change Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            ></Form.Control>
-          </Form.Group>
+    <>
+      <Row>
+        <Col md={3} className="mt-2">
+          <h3>Dados Pessoais</h3>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="username"
+                placeholder="Change Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-          <Form.Group controlId="name">
-            <Form.Label>Nome</Form.Label>
-            <Form.Control
-              type="name"
-              placeholder="nome"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></Form.Control>
-          </Form.Group>
+            <Form.Group controlId="name">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-          <Form.Group controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></Form.Control>
-          </Form.Group>
-        </Form>
-      </Col>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-      <Col md={9} className="mt-2">
-        <h3>Minhas Bicicletas</h3>
-        <Table striped bordered hover responsive className="table-sm">
-          <thead>
-            <tr>
-              <th>Marca</th>
-              <th>Modelo</th>
-              <th>Ano</th>
-              <th>Encontrada</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {bikes.map((bike) => (
-              <tr key={bike._id}>
-                <td>{bike.brand}</td>
-                <td>{bike.model}</td>
-                <td>{bike.year}</td>
-                <td>{bike.isRecovered}</td>
-                <td>
-                  {/* Create new page for detailing own bikes */}
-                  <LinkContainer to={`/bike/${bike._id}`}>
-                    <Button className="btn-sm" variant="light">
-                      Detalhes
-                    </Button>
-                  </LinkContainer>
-                </td>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        </Col>
+
+        <Col md={9} className="mt-2">
+          <h3>Minhas Bicicletas</h3>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>Marca</th>
+                <th>Modelo</th>
+                <th>Ano</th>
+                <th>Encontrada</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
+            </thead>
+            <tbody>
+              {bikes.map((bike) => (
+                <tr key={bike._id}>
+                  <td>{bike.brand}</td>
+                  <td>{bike.model}</td>
+                  <td>{bike.year}</td>
+                  <td>{bike.isRecovered}</td>
+                  <td>
+                    {/* Create new page for detailing own bikes */}
+                    <LinkContainer to={`/bike/${bike._id}`}>
+                      <Button className="btn-sm" variant="light">
+                        Detalhes
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={{ span: 3, offset: 10 }}>
+          <Button type="delete" variant="danger" className="mt-3 mb-3">
+            Delete Account
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 };
 
